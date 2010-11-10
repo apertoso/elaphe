722132eb276e74aceea6d5390fe22de07dbe1f17
whosaysni <whosaysni@gmail.com>
2010-11-10 14:31:34 +0000

    Refactord checdigits.py to checksums.py.

--- elaphe/util/checksum.py
+++ elaphe/util/checksum.py
@@ -1,12 +1,14 @@
 # coding: utf-8
+# Copyright (c) 2010 Yasushi Masuda. All rights reserved.
 """
 Various checksum algorithms
 
-Copyright (c) 2010 Yasushi Masuda. All rights reserved.
-
 """
+
+
 def modcomp(b, n):
-    """Computes complemented modulus (b, n).
+    """
+    Computes complemented modulus (b, n).
 
     >>> modcomp(1, 1), modcomp(1, 2), modcomp(1, 3), modcomp(2, 1), modcomp(2, 2), modcomp(2, 3)
     (0, 0, 0, 1, 0, 1)
@@ -15,157 +17,135 @@ def modcomp(b, n):
 
 
 def modulus_43(ordinals):
-    """Modulus-43, used in Code39.
+    """
+    Modulus-43, used in Code39.
 
     >>> modulus_43(translation.code39('ABCD1234+'))
     11
     """
     return sum(ordinals)%43
 
-# def modulus_10_w3 = 
 
-# class NumericOnlyCheckDigit(CheckDigit):
-#     MESSAGE_CODEC = CharMapCodec(string.digits)
+def modulus_10_w3(ordinals):
+    """
+    Modulus 10, weighted with 3. Used in JAN ITF and NW-7.
+
+    >>> modulus_10_w3(translation.digits('490123456789'))
+    4
+
+    """
+    return modcomp(
+        10, 
+        sum(ordinal*(1+2*(i%2)) 
+            for i, ordinal in enumerate(ordinals)))
 
 
-# class Modulus10W3(NumericOnlyCheckDigit):
-#     """Modulus 10, weighted with 3. Used in JAN ITF and NW-7.
+def modulus_16(ordinals):
+    """
+    Modulus 16, used in NW-7. Argument should include start/stop/checkdigit.
+  
+    >>> modulus_16(translation.nw7('A19+1243*B'))
+    12
+
+    """
+    return modcomp(16, sum(ordinals))
+
+
+def modulus_11(ordinals):
+    """
+    Modulus 11, used in NW-7. Argucment should exclude start/stop.
+
+    >>> modulus_11(translation.digits('2431245*'))
+    6
     
-#     >>> Modulus10W3().as_char('490123456789')
-#     '4'
-
-#     """
-#     def compute(self, ordinals, **kwargs):
-#         return modcomp(10, sum(ordinal*(1+2*(i%2)) for i, ordinal in enumerate(ordinals)))
-
-# modulus_10_w3 = Modulus10W3().as_char
+    """
+    return modcomp(
+        11, 
+        sum(ordinal*(7-i) 
+            for i, ordinal in enumerate(ordinals)
+            if i<6))
 
 
-# class Modulus16(CheckDigit):
-#     """Modulus 16, used in NW-7. Argument should include start/stop/checkdigit.
-    
-#     >>> Modulus16().as_char('A19+1243*B')
-#     ':'
+def modulus_10_w2(ordinals):
+    """
+    Modulus 10, weighted with 2. Used in NW-7. 
+    Argument should exclude start/stop/checkdigit.
 
-#     """
-#     MESSAGE_CODEC = CharMapCodec(string.digits+'-$:/.+ABCD')
-
-#     def normalize_message(self, message):
-#         list_chars = list(message)
-#         return (c.upper() for i, c in enumerate(list_chars) if i!=len(list_chars)-2)
-
-#     def compute(self, ordinals, **kwargs):
-#         return modcomp(16, sum(ordinals))
-
-# modulus_16 = Modulus16().as_char
+    >>> modulus_10_w2(translation.digits('938745343'))
+    7
+    """
+    return modcomp(
+        10, 
+        sum(ordinal*(2-(i%2)) 
+            for i, ordinal in enumerate(ordinals)))
 
 
-# class Modulus11(NumericOnlyCheckDigit):
-#     """Modulus 11, used in NW-7. Argucment should exclude start/stop.
-    
-#     >>> Modulus11().as_char('2431245*')
-#     '6'
+def check_dr_7(ordinals):
+    """
+    7-check DR checkdigit, used in NW-7. 
+    Argument should exclude start/stop/checkdigit.
 
-#     """
-#     def normalize_message(self, message):
-#         return message[:6]
+    >>> check_dr_7(translation.digits('8745343'))
+    5
 
-#     def compute(self, ordinals, **kwargs):
-#         return modcomp(11, sum(ordinal*(7-i) for i, ordinal in enumerate(ordinals)))
-
-# modulus_11 = Modulus11().as_char
+    """
+    return sum(
+        (10**(6-i)*ordinal)
+        for i, ordinal in enumerate(ordinals) if i<7)%7
 
 
-# class Modulus10w2(NumericOnlyCheckDigit):
-#     """Modulus 10, weighted with 2. Used in NW-7. Argument should exclude start/stop/checkdigit.
+def weighted_modulus_11(ordinals):
+    """
+    Weighted modulus 11, used in NW-7. Argument should exclude start/stop.
 
-#     >>> modulus_10_w2('938745343')
-#     '7'
+    >>> weighted_modulus_11(translation.digits('5012924346'))
+    4
+    >>> weighted_modulus_11(translation.digits('0200290068'))
+    1
 
-#     """
-#     def compute(self, ordinals, **kwargs):
-#         return modcomp(10, sum(ordinal*(2-(i%2)) for i, ordinal in enumerate(ordinals)))
-
-# modulus_10_w2 = Modulus10w2().as_char
-
-
-# class CheckDR7(NumericOnlyCheckDigit):
-#     """7-check DR checkdigit, used in NW-7. Argument should exclude start/stop/checkdigit.
-
-#     >>> check_DR_7('8745343')
-#     '5'
-
-#     """
-#     def as_ordinal(self, message, errors=None, **kwargs):
-#         return int(''.join(message), 10)%7
-
-#     def as_char(self, message, errors=None, **kwargs):
-#         return ''.join(self.message_codec.encode([self.as_ordinal(message, errors, **kwargs)]))
-
-# check_DR_7 = CheckDR7().as_char
+    """
+    WEIGHTS_1 = [6, 3, 5, 9, 10, 7, 8, 4, 5, 3, 6, 2]
+    WEIGHTS_2 =  [5, 8, 6, 2, 10, 4, 3, 7, 6, 8, 5, 9]
+    olist = list(ordinals)[::-1]
+    mod = sum(ordinal*WEIGHTS_1[-1-i] 
+              for i, ordinal in enumerate(olist))%11
+    if mod==0:
+        return 0
+    elif mod==1:
+        return modcomp(
+            11, 
+            sum(ordinal*WEIGHTS_2[-1-i] 
+                for i, ordinal in enumerate(olist)))
+    else:
+        return modcomp(11, mod)
 
 
-# class WeightedModulus11(NumericOnlyCheckDigit):
-#     """Weighted modulus 11, used in NW-7. Argument should exclude start/stop.
+def runes(ordinals):
+    """
+    RUNES modulus 10, weighted with 2. Used in NW-7.
 
-#     >>> weighted_modulus_11('5012924346')
-#     '4'
-#     >>> weighted_modulus_11('0200290068')
-#     '1'
+    >>> runes(translation.digits('938745343'))
+    5
 
-#     """
-#     WEIGHT_1 = [6, 3, 5, 9, 10, 7, 8, 4, 5, 3, 6, 2]
-#     WEIGHT_2 =  [5, 8, 6, 2, 10, 4, 3, 7, 6, 8, 5, 9]
-
-#     def compute(self, ordinals, **kwargs):
-#         olist = list(ordinals)
-#         mod = sum(ordinal*self.WEIGHT_1[-1-i] for i, ordinal in enumerate(olist[::-1]))%11
-#         if mod==0:
-#             return 0
-#         elif mod==1:
-#             return modcomp(11, sum(ordinal*self.WEIGHT_2[-1-i] for i, ordinal in enumerate(olist[::-1])))
-#         else:
-#             return modcomp(11, mod)
-
-# weighted_modulus_11 = WeightedModulus11().as_char
+    """
+    return modcomp(
+        10, 
+        sum(sum(divmod(ordinal*(2-(i%2)), 10)) 
+            for i, ordinal in enumerate(ordinals)))
 
 
-# class Runes(NumericOnlyCheckDigit):
-#     """RUNES modulus 10, weighted with 2. Used in NW-7.
+def modulus_103(ordinals):
+    """
+    Modulus 103, used in code128. 
+    Code should include start char and exclude stop char.
 
-#     >>> runes('938745343')
-#     '5'
+    >>> modulus_103(translation.code128('^105^102123456^100A1'))
+    35
 
-#     """
-#     def compute(self, ordinals, **kwargs):
-#         return modcomp(10, sum(sum(divmod(ordinal*(2-(i%2)), 10)) for i, ordinal in enumerate(ordinals)))
-
-# runes = Runes().as_char
-
-
-# class Modulus103(CheckDigit):
-#     """Modulus 103, used in code128. Code should include start char and exclude stop char.
-
-#     >>> modulus_103('^105^102123456^100A1')
-#     '35'
-
-#     """
-#     MESSAGE_CODEC = Code128Codec()
-
-#     def compute(self, ordinals, **kwargs):
-#         csum = 0
-#         for i, ordinal in enumerate(ordinals):
-#             if i==0:
-#                 csum = ordinal
-#             else:
-#                 csum += i*ordinal
-#         return csum%103
-
-#     def as_char(self, message, code=None, errors=None, **kwargs):
-#         return ''.join(self.message_codec.encode([self.as_ordinal(message, errors, **kwargs)], code=code))
-    
-# _modulus_103 = Modulus103()
-# modulus_103 = lambda s: _modulus_103.as_char(s, code='C')
+    """
+    return sum(
+        (ordinal if i==0 else i*ordinal)
+        for i, ordinal in enumerate(ordinals))%103
 
 
 if __name__=='__main__':
