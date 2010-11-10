ce760c46bdb697563b0dc8c0abf1751f98dc5c72
whosaysni <whosaysni@gmail.com>
2010-11-10 05:35:04 +0000

    Added Code128Translation.

--- elaphe/util/translation.py
+++ elaphe/util/translation.py
@@ -49,11 +49,15 @@ class Translation(object):
     def __init__(self, **kwargs):
         """Constructor.
         """
-        return
+
+    def reset(self):
+        """Resets internal status. Subclass may override.
+        """
 
     def translate(self, message, on_error=None, **kwargs):
         """Converts sequence of ordinals to message string.
         """
+        self.reset()
         queue = []
         for char in message:
             queue.append(char)
@@ -131,17 +135,19 @@ class CharMapTranslation(Translation):
     [104, 108, 102, 106]
 
     """
-    def __init__(self, chars, offset=0, skip_char=None):
+    def __init__(self, chars, offset=0, skip_char=None, **kwargs):
         """Constructor.
 
         Accepts a positional argument ``char``, and two optional
         parameters: ``offset`` and ``skip_char``.
         """
+        super(CharMapTranslation, self).__init__(**kwargs)
         self.allowed_chars = chars
         if skip_char:
             self.allowed_chars.replace(skip_char, '')
         self.map = dict(
-            (char, idx+offset) for idx, char in enumerate(chars)
+            (char, idx+offset) 
+            for idx, char in enumerate(chars) 
             if char!=skip_char)
 
     def translate_chars(self, chars):
@@ -152,8 +158,6 @@ class CharMapTranslation(Translation):
         """
         if len(chars)>1:
             raise TranslationError(u'Multiple characters not allowed.')
-        elif len(chars)<1:
-            return
         # else
         char = chars[0]
         if char not in self.map:
@@ -163,6 +167,96 @@ class CharMapTranslation(Translation):
         # else
         return self.map.get(char)
         
+
+class Code128Translation(Translation):
+    """Converts message to code128 ordinals. Accepts cap('^')-escaped non-printables.
+
+    >>> c128t = Code128Translation()
+    >>> c128t # doctest: +ELLIPSIS
+    <...Code128Translation object at ...>
+
+    Escaped characters are processed successfully.
+    >>> list(c128t.translate('^103AZ_^^_\\0\\1\\2', on_error=Translation.RAISE_ON_ERROR))
+    [103, 33, 58, 63, 62, 63, 64, 65, 66]
+    >>> list(c128t.translate('^104AZ_^^_\\x60ab', on_error=Translation.RAISE_ON_ERROR))
+    [104, 33, 58, 63, 62, 63, 64, 65, 66]
+    >>> list(c128t.translate('^105^102^', on_error=Translation.RAISE_ON_ERROR))
+    [105, 102]
+    >>> list(c128t.translate('^105^102123456^100A1', on_error=Translation.RAISE_ON_ERROR))
+    [105, 102, 12, 34, 56, 100, 33, 17]
+    >>> list(c128t.translate('^105^102123456^100A1', on_error=Translation.RAISE_ON_ERROR))
+    [105, 102, 12, 34, 56, 100, 33, 17]
+
+    Invalid character raises TranslationError (provided on_error is set).
+    >>> list(c128t.translate('^105^X', on_error=Translation.RAISE_ON_ERROR))
+    Traceback (most recent call last):
+    ...
+    TranslationError: ^X is not allowed for Code128 escape sequence.
+    >>> list(c128t.translate('^105^X^102'))
+    [105, 102]
+    
+    """
+    ASCII_7BITS = ''.join(chr(i) for i in range(128))
+    CODE_A_TABLE = ASCII_7BITS[32:96] + ASCII_7BITS[:32]
+    CODE_B_TABLE = ASCII_7BITS[32:128]
+    CODE_TABLES = dict(A=CODE_A_TABLE, B=CODE_B_TABLE)
+
+    def __init__(self, **kwargs):
+        super(Code128Translation, self).__init__(**kwargs)
+        self.code = None
+
+    def reset(self):
+        """Resets internal code status.
+        """
+        self.code = None
+
+    def translate_chars(self, chars):
+        if chars[0] == '^':
+            if len(chars)==1:
+                # escape in
+                return
+            else:
+                # escaped already
+                if chars[1] == '^':
+                    return 62
+                else:
+                    escaped = ''.join(chars[1:])
+                    if escaped.isdigit():
+                        ordinal = int(escaped, 10)
+                        if ordinal<96:
+                            return
+                        elif ordinal in range(96, 108):
+                            if ordinal in (99, 105):
+                                self.code = 'C'
+                            elif ordinal in (100, 104):
+                                self.code = 'B'
+                            elif ordinal in (101, 103):
+                                self.code = 'A'
+                            return ordinal
+                        else: 
+                            # ordinal larger than 108
+                            pass
+                    # ordinal larger than 108, or invalid escape sequence
+                    raise TranslationError(
+                        u'^%s is not allowed for Code128 escape sequence.' 
+                        %(escaped))
+        else:
+            ordinal = None
+            if self.code in ['A', 'B']:
+                code_table = self.CODE_TABLES[self.code]
+                try:
+                    ordinal = code_table.index(chars[0])
+                except IndexError:
+                    raise TranslationError(
+                        u'%(char)s is not in code table %(code)s'
+                        %(dict(char=chars[0], code=self.code)))
+            elif self.code == 'C':
+                # requires two chars to continue
+                if len(chars)==2:
+                    ordinal = int(''.join(chars), 10)
+                else:
+                    return
+            return ordinal
 
 
 if __name__=="__main__":
