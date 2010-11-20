7dfbc7c745fe364b5475c38b9612263cd5f67bfa
whosaysni <whosaysni@gmail.com>
2010-11-20 13:30:07 +0000

    Removed Code128Translation, Translation now accepts sequence of ordinals for mapped values.

--- elaphe/symbology/util/translation.py
+++ elaphe/symbology/util/translation.py
@@ -25,19 +25,23 @@ class TranslationError(ValueError):
 
 
 # mapping utility
-def charmap(chars, offset=0, skip_char=None):
+def charmap(chars, ordinals=None):
     """
     Generate character map for given chars.
 
-    ``offset`` and ``skip_char`` are for non-ordinal alphabets.
-    >>> cm = charmap('0*1*2*3*4', offset=100, skip_char='*')
-    >>> sorted(cm.items()) # doctest: +ELLIPSIS
-    [('*', None), ('0', 100), ('1', 102), ('2', 104), ('3', 106), ('4', 108)]
-
+    >>> cm1 = charmap('012ABCD')
+    >>> sorted(cm1.items()) # doctest: +ELLIPSIS
+    [('0', 0), ('1', 1), ('2', 2), ('A', 3), ('B', 4), ('C', 5), ('D', 6)]
+    
+    If ``ordinals`` is given, it will be mapped to chars.
+    >>> oz = [ord(c) for c in 'abAB+/.']
+    >>> cm2 = charmap('abAB+/.', ordinals=oz)
+    >>> sorted(cm2.items()) # doctest: +ELLIPSIS
+    [('+', 43), ('.', 46), ('/', 47), ('A', 65), ... ('b', 98)]
     """
-    return dict(
-        (char, (idx+offset if char!=skip_char else None))
-        for idx, char in enumerate(chars))
+    if ordinals==None:
+        ordinals = range(len(chars))
+    return dict(zip(chars, ordinals))
 
 
 class Translation(object):
@@ -82,9 +86,9 @@ class Translation(object):
         queue = ''
         for char in message:
             queue += char
-            ordinal = None
+            ordinal_info = None
             try:
-                ordinal = self.translate_chars(queue)
+                ordinal_info = self.translate_chars(queue)
             except TranslationError, e:
                 if on_error == self.RAISE_ON_ERROR:
                     raise e
@@ -99,16 +103,22 @@ class Translation(object):
                     queue = ''
             except:
                 raise
-            if ordinal is None:
+            if ordinal_info is None:
                 # wait for next char
                 continue
-            elif ordinal is NotImplemented:
+            elif ordinal_info is NotImplemented:
                 raise TypeError(u'This type has no valid '
                                 'translate_chars() implementation.')
             else:
                 # clear queue, yield something not None.
                 queue = ''
-                yield ordinal
+                if isinstance(ordinal_info, (list, tuple)):
+                    # iterate if ordinal_info is sequence
+                    for ordinal in ordinal_info:
+                        yield ordinal
+                else:
+                    # ... or return it directly
+                    yield ordinal_info
     
     def translate_chars(self, chars):
         """
@@ -123,7 +133,7 @@ class Translation(object):
 
 class MapTranslation(Translation):
     """
-    Translation using character map.
+    Translation using a map.
 
     >>> cmt = MapTranslation(charmap('+0123456789'))
     >>> cmt # doctest: +ELLIPSIS
@@ -161,8 +171,8 @@ class MapTranslation(Translation):
         """
         Constructor.
 
-        Accepts a positional argument ``char``, and two optional
-        parameters: ``offset`` and ``skip_char``.
+        Accepts a positional argument ``char``, and three optional
+        parameters: min_chars, max_chars, and extra_map.
 
         """
         super(MapTranslation, self).__init__(**kwargs)
@@ -191,148 +201,8 @@ class MapTranslation(Translation):
             return self.map.get(chars)
 
 
-class Code128Translation(Translation):
-    """
-    Converts message to code128 ordinals. Accepts cap('^')-escaped non-printables.
-
-    >>> c128t = Code128Translation()
-    >>> c128t # doctest: +ELLIPSIS
-    <...Code128Translation object at ...>
-
-    Escaped characters are processed successfully.
-    >>> list(c128t.translate('^103AZ_^^_\\0\\1\\2', on_error=Translation.RAISE_ON_ERROR))
-    [103, 33, 58, 63, 62, 63, 64, 65, 66]
-    >>> list(c128t.translate('^104AZ_^^_\\x60ab', on_error=Translation.RAISE_ON_ERROR))
-    [104, 33, 58, 63, 62, 63, 64, 65, 66]
-    >>> list(c128t.translate('^105^102^', on_error=Translation.RAISE_ON_ERROR))
-    [105, 102]
-    >>> list(c128t.translate('^105^102123456^100A1', on_error=Translation.RAISE_ON_ERROR))
-    [105, 102, 12, 34, 56, 100, 33, 17]
-    >>> list(c128t.translate('^105^102123456^100A1', on_error=Translation.RAISE_ON_ERROR))
-    [105, 102, 12, 34, 56, 100, 33, 17]
-
-    Invalid character raises TranslationError (provided on_error is set).
-    >>> list(c128t.translate('^105^X', on_error=Translation.RAISE_ON_ERROR))
-    Traceback (most recent call last):
-    ...
-    TranslationError: ^X is not allowed for Code128 escape sequence.
-    >>> list(c128t.translate('^105^X^102'))
-    [105, 102]
-    
-    """
-    ASCII_7BITS = ''.join(chr(i) for i in range(128))
-    CODE_A_TABLE = ASCII_7BITS[32:96] + ASCII_7BITS[:32]
-    CODE_B_TABLE = ASCII_7BITS[32:128]
-    CODE_TABLES = dict(A=CODE_A_TABLE, B=CODE_B_TABLE)
-
-    @staticmethod
-    def cap_escape(s):
-        """
-        Converts string to cap-escaped printables.
-
-        >>> cap_escape = Code128Translation.cap_escape
-        >>> cap_escape('abcdef')
-        'abcdef'
-        >>> cap_escape('\\1\\2\\3\\10\\20\\30\\110\\120\\130\\200\\210\\220\\240')
-        '^1^2^3^8^16^24HPX^128^136^144^160'
-
-        """
-        return ''.join(c if c in string.printable else '^'+str(ord(c))
-                       for c in s)
-
-    def __init__(self, **kwargs):
-        super(Code128Translation, self).__init__(**kwargs)
-        self.code = None
-
-    def reset(self):
-        """Resets internal code status.
-        """
-        self.code = None
-
-    def translate_chars(self, chars):
-        """
-        Translates characters according to Code128 rules.
-
-        The method employs internal mode stored in ``code`` instance attribute.
-        
-        >>> c128t = Code128Translation()
-        >>> c128t.code # None
-        >>> c128t.translate_chars(list('^')) # None
-        >>> c128t.translate_chars(list('^^'))
-        62
-
-        # Shift code to A
-        >>> c128t.translate_chars(list('^101'))
-        101
-        >>> c128t.code
-        'A'
-        >>> c128t.translate_chars(list('Z'))
-        58
-
-        # Shift code to B
-        >>> c128t.translate_chars(list('^104'))
-        104
-        >>> c128t.code
-        'B'
-
-        # Shift code to C
-        >>> c128t.translate_chars(list('^99'))
-        99
-        >>> c128t.code
-        'C'
-
-        """
-        if chars[0]=='^':
-            if len(chars)==1:
-                # escape in
-                return
-            else:
-                # escaped already
-                if chars[1]=='^':
-                    return 62
-                else:
-                    escaped = ''.join(chars[1:])
-                    if escaped.isdigit():
-                        ordinal = int(escaped, 10)
-                        if ordinal<96:
-                            return
-                        elif ordinal in range(96, 108):
-                            if ordinal in (99, 105):
-                                self.code = 'C'
-                            elif ordinal in (100, 104):
-                                self.code = 'B'
-                            elif ordinal in (101, 103):
-                                self.code = 'A'
-                            return ordinal
-                        else: 
-                            # ordinal larger than 108
-                            pass
-                    # ordinal larger than 108, or invalid escape sequence
-                    raise TranslationError(
-                        u'^%s is not allowed for Code128 escape sequence.' 
-                        %(escaped))
-        else:
-            ordinal = None
-            if self.code in ['A', 'B']:
-                code_table = self.CODE_TABLES[self.code]
-                try:
-                    ordinal = code_table.index(chars[0])
-                except IndexError:
-                    raise TranslationError(
-                        u'%(char)s is not in code table %(code)s'
-                        %(dict(char=chars[0], code=self.code)))
-            elif self.code == 'C':
-                # requires two chars to continue
-                if len(chars)==2:
-                    ordinal = int(''.join(chars), 10)
-                else:
-                    return
-            return ordinal
-
-
 # common translation shortcuts 
 digits = MapTranslation(charmap('0123456789')).translate
-code128 = Code128Translation().translate
 
 
 if __name__=="__main__":
